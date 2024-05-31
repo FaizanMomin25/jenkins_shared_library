7786529e209d6e1c1483b99d498bef2480060282
Faizan Momin <121131867+FaizanMomin25@users.noreply.github.com>
2024-05-31 14:19:58 +0530

    added DeliveryPipeline code

--- vars/DockerDeliveryPipeline.groovy
+++ vars/DockerDeliveryPipeline.groovy
@@ -1,1 +1,16 @@
-hi
+pipeline {
+    agent any
+    stages {
+        stage('Example') {
+            steps {
+                script {
+                    getPlatformName()
+                }
+            }
+        }
+    }
+}
+
+def getPlatformName() {
+    return config.platform
+}
