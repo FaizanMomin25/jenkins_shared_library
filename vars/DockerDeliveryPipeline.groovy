ec149a78674e13944d2a6a51df82ef71530ad788
Faizan Momin <121131867+FaizanMomin25@users.noreply.github.com>
2024-05-31 17:01:58 +0530

    Update DockerDeliveryPipeline.groovy

--- vars/DockerDeliveryPipeline.groovy
+++ vars/DockerDeliveryPipeline.groovy
@@ -1,36 +1,44 @@
-pipeline {
-    agent any 
-    environment {
-        registryURI = "https://registry.hub.docker.com/"
-        registry = "faizanmomin2508/faizan_cloudethix_nginx"
-        registryCredential = '01_docker_Hub_creds'
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    pipeline {
+        agent any 
+        environment {
+            registryURI = "https://registry.hub.docker.com/"
+            registry = "faizanmomin2508/faizan_cloudethix_nginx"
+            registryCredential = '01_docker_Hub_creds'
         }
-stages {
-        stage('Building image from project dir from shared library') {
-            environment {
-                registry_endpoint = "${env.registryURI}" + "${env.registry}"
-                tag_commit_id     = "${env.registry}" + ":$GIT_COMMIT"
+        stages {
+            stage('Building image from project dir from shared library') {
+                environment {
+                    registry_endpoint = "${env.registryURI}${env.registry}"
+                    tag_commit_id = "${env.registry}:$GIT_COMMIT"
+                }
+                steps {
+                    script {
+                        def app = docker.build(tag_commit_id)
+                        docker.withRegistry(registry_endpoint, registryCredential) {
+                            app.push()
+                        }
+                    }
+                }
             }
-            steps{
-                script {
-                def app = docker.build(tag_commit_id)
-                docker.withRegistry( registry_endpoint, registryCredential ) {
-                app.push()
+            stage('Remove Unused docker image from shared library') {
+                steps {
+                    sh "docker rmi $registry:$GIT_COMMIT"
                 }
             }
         }
-    }
-
-        stage('Remove Unused docker image from shared library') {
-            steps{
-                sh "docker rmi $registry:$GIT_COMMIT"
-                }
+        post { 
+            always { 
+                echo 'Deleting Workspace Dir from shared library'
+                deleteDir() /* clean up our workspace */
+            }
         }
     }
-    post { 
-        always { 
-            echo 'Deleting Workspace Dir from shared library'
-            deleteDir() /* clean up our workspace */
-        }
+    def getPlatformName() {
+        return config.platform
     }
 }
