1d9f7e92b849e1930f93b9d59ab70374f1d6a5cd
Faizan Momin <121131867+FaizanMomin25@users.noreply.github.com>
2024-05-31 16:23:45 +0530

    Update DockerDeliveryPipeline.groovy

--- vars/DockerDeliveryPipeline.groovy
+++ vars/DockerDeliveryPipeline.groovy
@@ -1,16 +1,36 @@
 pipeline {
-    agent any
-    stages {
-        stage('Example') {
-            steps {
+    agent any 
+    environment {
+        registryURI = "https://registry.hub.docker.com/"
+        registry = "faizanmomin2508/faizan_cloudethix_nginx"
+        registryCredential = '01_docker_Hub_creds'
+        }
+stages {
+        stage('Building image from project dir from shared library') {
+            environment {
+                registry_endpoint = "${env.registryURI}" + "${env.registry}"
+                tag_commit_id     = "${env.registry}" + ":$GIT_COMMIT"
+            }
+            steps{
                 script {
-                    getPlatformName()
+                def app = docker.build(tag_commit_id)
+                docker.withRegistry( registry_endpoint, registryCredential ) {
+                app.push()
                 }
             }
         }
     }
-}
 
-def getPlatformName() {
-    return config.platform
+        stage('Remove Unused docker image from shared library') {
+            steps{
+                sh "docker rmi $registry:$GIT_COMMIT"
+                }
+        }
+    }
+    post { 
+        always { 
+            echo 'Deleting Workspace Dir from shared library'
+            deleteDir() /* clean up our workspace */
+        }
+    }
 }
